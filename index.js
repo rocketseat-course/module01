0ceb0a4f543cdcf67ed84cd3f4d83984268382f3
Carlos Mestre <carlos.mestre@gamersclub.com.br>
2020-02-25 11:55:45 -0300

    query and route params example

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const express = require('express');
 
 const server = express();
 
+// Query params = ?test=1
+// Route params = /test/1
+// Request body = { "name": "Vinicius", "locale": "pt_BR" }
+
 server.get('/test', (req, res) => {
     // return res.send('Hello World!');
-    return res.json({ message: 'Hello World!' });
+    const name = req.query.name === undefined ? "World" : req.query.name;
+    return res.json({ message: `Hello ${name}` });
+});
+
+server.get('/users/:id', (req, res) => {
+    const { id } = req.params;
+    return res.json({ message: `Searching for user ${id}` });
 });
 
 server.listen(3000);
